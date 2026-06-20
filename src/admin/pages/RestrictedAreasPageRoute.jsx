92ceb982a34bfde24918534e8fac7edf7239ae09
mustafahajabdn-wq <mustafa.haj.abd.n@gmail.com>
2026-06-20 06:17:32 +0200

    Route geographic management page

--- src/admin/pages/RestrictedAreasPageRoute.jsx
+++ src/admin/pages/RestrictedAreasPageRoute.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Navigate } from "react-router-dom";
-import RestrictedAreasPage from "./RestrictedAreasPage.jsx";
+import LocationsPage from "./LocationsPage.jsx";
 
 export default function RestrictedAreasPageRoute(props) {
   const { user } = props;
@@ -12,5 +12,5 @@ export default function RestrictedAreasPageRoute(props) {
     return <Navigate to="/admin/dashboard" replace />;
   }
 
-  return <RestrictedAreasPage {...props} />;
+  return <LocationsPage {...props} />;
 }
