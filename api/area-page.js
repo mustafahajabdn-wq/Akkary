1baef28950cf8d1e10d68141f5b7dbe4cb844bd3
mustafahajabdn-wq <mustafa.haj.abd.n@gmail.com>
2026-06-30 15:53:14 +0200

    Use API-local category slug helper

--- api/area-page.js
+++ api/area-page.js
@@ -1,6 +1,6 @@
 import fs from "node:fs";
 import path from "node:path";
-import { getCategoryTypeDefinition } from "../src/shared/seo/categoryTypeSlugs.js";
+import { getCategoryTypeDefinition } from "./seoCategoryTypeSlugs.js";
 
 const SITE_URL = "https://www.blabladar.com";
 const MIN_AREA_LISTINGS = 1;
